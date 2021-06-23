756d9d0a3a84079490fe0f10264a8d36fa81b7f4
Yudo Kartiko <44739836+yudoBinus@users.noreply.github.com>
2021-06-23 15:35:08 +0700

    Sg 004 (#5)
    * Add user model with validate
    
    * create Volunteer model with validate
    
    * add job model with validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_082030) do
+ActiveRecord::Schema.define(version: 2021_06_23_082710) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "natural_disasters", force: :cascade do |t|
     t.string "name"
