f3f9c21a3c95ae5b63c50165e91e612c0a765267
Yudo Kartiko <yudobinus@gmail.com>
2021-06-23 23:10:07 +0700

    add button to report_form and create basic form

--- app/controllers/natural_disasters_controller.rb
+++ app/controllers/natural_disasters_controller.rb
@@ -2,4 +2,17 @@ class NaturalDisastersController < ApplicationController
   def show
     @natural_disaster = NaturalDisaster.find_by(id: params[:id])
   end
+
+  def report
+    @natural_disaster = NaturalDisaster.new(
+      name: params[:name], 
+      location: params[:location], 
+      desc: params[:desc])
+    
+    if @natural_disaster.save 
+      redirect_to("/natural_disasters/#{@natural_disaster.id}/show")
+    else
+      render("natural_disaster")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'home/index'
   
   get 'natural_disasters/:id/show' => 'natural_disasters#show'
+  get 'natural_disasters/report-form' => 'natural_disasters#report-form'
+  post 'natural_disasters/report' => 'natural_disasters#report'
 end
