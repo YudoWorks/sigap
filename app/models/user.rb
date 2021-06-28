8b00b62884f4317dcb412412ca5480539a560b2b
Yudo Kartiko <44739836+yudoBinus@users.noreply.github.com>
2021-06-28 17:33:01 +0700

    fixing password bug (#13)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,8 @@
 require 'bcrypt'
 
 class User < ApplicationRecord
-    include BCrypt
+    has_secure_password
+
     validates :name, {presence: true}
     validates :email, {presence: true, uniqueness: true}
-
-    def password
-        @password ||= Password.new(password_hash)
-    end
-
-    def password=(new_password)
-        @password = Password.create(new_password)
-        self.password_hash = @password
-    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_28_095946) do
+ActiveRecord::Schema.define(version: 2021_06_28_102758) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "name"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_06_28_095946) do
     t.boolean "isAdmin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_hash"
+    t.string "password_digest"
   end
 
   create_table "volunteers", force: :cascade do |t|
